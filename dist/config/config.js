756720cb2e47ea2b8bc40def4fac98276701bc04
Nikola Tasic <ntasic7@gmail.com>
2018-12-03 15:43:19 +0100

    linux config check

--- dist/config/config.js
+++ dist/config/config.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var fs_1 = require("fs");
 var path_1 = require("path");
 var child_process_1 = require("child_process");
+var os_1 = require("os");
 var configJSON = JSON.parse(fs_1.readFileSync(path_1.join(process.cwd(), "dist/config/config.json"), { encoding: "utf8" }));
 if (configJSON.db.path.length == 0)
     configJSON.db.path = path_1.join(process.cwd(), "dist/database/db");
@@ -13,9 +14,14 @@ if (configJSON.db.uploads.length == 0)
 if (configJSON.db.defaultProduct.length == 0)
     configJSON.db.defaultProduct = path_1.join(process.cwd(), "dist/config/product.png");
 if (configJSON.mongod.path.length == 0)
-    configJSON.mongod.path = child_process_1.execSync("where mongod")
-        .toString()
-        .split("\r\n")[0];
+    configJSON.mongod.path =
+        os_1.platform() == "win32"
+            ? child_process_1.execSync("where mongod")
+                .toString()
+                .split("\r\n")[0]
+            : child_process_1.execSync("which mongod")
+                .toString()
+                .split("\r\n")[0];
 if (configJSON.mongod.conf.length == 0)
     configJSON.mongod.conf = path_1.join(process.cwd(), "dist/config/mongod.cfg");
 if (isNaN(configJSON.serverPort))
--- src/config/config.ts
+++ src/config/config.ts
@@ -2,9 +2,10 @@ import { SchemaOptions, Schema } from "mongoose";
 import { readFileSync, existsSync, mkdirSync } from "fs";
 import { join } from "path";
 import { generate } from "shortid";
-import { execSync } from "child_process";
+import { execSync, exec } from "child_process";
 import { createUser } from "../models/User";
 import Admin, { adminDefinition } from "../models/Admin";
+import { platform } from "os";
 export interface Collections {
 	users?: string;
 	products?: string;
@@ -29,9 +30,14 @@ if (configJSON.db.uploads.length == 0) configJSON.db.uploads = join(process.cwd(
 if (configJSON.db.defaultProduct.length == 0)
 	configJSON.db.defaultProduct = join(process.cwd(), "dist/config/product.png");
 if (configJSON.mongod.path.length == 0)
-	configJSON.mongod.path = execSync("where mongod")
-		.toString()
-		.split("\r\n")[0];
+	configJSON.mongod.path =
+		platform() == "win32"
+			? execSync("where mongod")
+					.toString()
+					.split("\r\n")[0]
+			: execSync("which mongod")
+					.toString()
+					.split("\r\n")[0];
 if (configJSON.mongod.conf.length == 0) configJSON.mongod.conf = join(process.cwd(), "dist/config/mongod.cfg");
 if (isNaN(configJSON.serverPort)) configJSON.serverPort = 3000;
 if (!existsSync(configJSON.db.path)) mkdirSync(configJSON.db.path, { recursive: true });
